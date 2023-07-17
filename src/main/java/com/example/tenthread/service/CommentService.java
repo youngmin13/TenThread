fc56dcbd43d919b3c609209ab0f4574f22d2f09f
SeungBum Baek <wer06099@naver.com>
2023-07-17 20:47:48 +0900

    주석제거 및 게시글 User 체크

--- src/main/java/com/example/tenthread/service/CommentService.java
+++ src/main/java/com/example/tenthread/service/CommentService.java
@@ -4,8 +4,10 @@ import com.example.tenthread.dto.ApiResponseDto;
 import com.example.tenthread.dto.CommentRequestDto;
 import com.example.tenthread.entity.Comment;
 import com.example.tenthread.entity.Post;
+import com.example.tenthread.entity.User;
 import com.example.tenthread.repository.CommentRepository;
 import com.example.tenthread.repository.PostRepository;
+import com.example.tenthread.repository.UserRepository;
 import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,16 +18,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CommentService {
     //todo
-    /*private final UserRepository userRepository;*/
+    private final UserRepository userRepository;
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
 
-    public ApiResponseDto createComment(Long postId, CommentRequestDto requestDto/*, User user*/){
+    public ApiResponseDto createComment(Long postId, CommentRequestDto requestDto, User user){
 
-        /*userRepository.findById(user.getId()).orElseThrow(
+        userRepository.findById(user.getId()).orElseThrow(
                 () -> new NullPointerException("해당 회원이 존재하지 않습니다.")
         );
-*/
+
         Post post = postRepository.findById(postId).orElseThrow(
                 () -> new NullPointerException("해당 댓글이 존재하지 않습니다.")
         );
@@ -39,11 +41,11 @@ public class CommentService {
 
 
     @Transactional
-    public ApiResponseDto updateComment(Long commentId, CommentRequestDto requestDto/*, User user*/) {
-        /*userRepository.findById(user.getId()).orElseThrow(
+    public ApiResponseDto updateComment(Long commentId, CommentRequestDto requestDto, User user) {
+        userRepository.findById(user.getId()).orElseThrow(
                 () -> new NullPointerException("해당 회원이 존재하지 않습니다.")
         );
-*/
+
 
         Comment comment = commentRepository.findById(commentId).orElseThrow(
                 () -> new NullPointerException("해당 댓글이 존재하지 않습니다.")
@@ -56,11 +58,11 @@ public class CommentService {
     }
 
 
-    public ApiResponseDto deleteComment(Long commentId, CommentRequestDto requestDto/*, User user*/) {
-                /*userRepository.findById(user.getId()).orElseThrow(
+    public ApiResponseDto deleteComment(Long commentId, User user) {
+                userRepository.findById(user.getId()).orElseThrow(
                 () -> new NullPointerException("해당 회원이 존재하지 않습니다.")
         );
-*/
+
         Comment comment = commentRepository.findById(commentId).orElseThrow(
                 () -> new NullPointerException("해당 댓글이 존재하지 않습니다.")
         );
