a54d2959d6f68a2e065f34d47a09d926ac0da826
SeungBum Baek <wer06099@naver.com>
2023-07-17 15:43:35 +0900

    게시글 단건조회 및 삭제 및 예외처리 작업

--- src/main/java/com/example/tenthread/controller/PostController.java
+++ src/main/java/com/example/tenthread/controller/PostController.java
@@ -1,9 +1,12 @@
 package com.example.tenthread.controller;
 
+import com.example.tenthread.dto.ApiResponseDto;
 import com.example.tenthread.dto.PostRequestDto;
 import com.example.tenthread.dto.PostResponseDto;
 import com.example.tenthread.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +14,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 public class PostController {
     private final PostService postService;
+
+    @GetMapping("post/{postId}")
+    public PostResponseDto getPost(@PathVariable Long postId) {
+        return postService.getPost(postId);
+    }
 
     @PostMapping("post")
     public PostResponseDto createPost(@RequestBody PostRequestDto requestDto) {
@@ -20,5 +28,19 @@ public class PostController {
     @PutMapping("post/{postId}")
     public PostResponseDto updatePost(@RequestBody PostRequestDto requestDto, @PathVariable Long postId) {
         return postService.updatePost(requestDto, postId);
+    }
+
+    @DeleteMapping("post/{postId}")
+    public ApiResponseDto deletePost(@PathVariable Long postId) {
+        return postService.deletePost(postId);
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<ApiResponseDto> handleException(IllegalArgumentException ex) {
+        ApiResponseDto restApiException = new ApiResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                restApiException,
+                HttpStatus.BAD_REQUEST
+        );
     }
 }
