ba75ae15ffeb69bd4c736e8673779eda36787cb1
yuio7279 <yuio7279@gmail.com>
2023-07-18 05:19:06 +0900

    CommentController 주석 추가

--- src/main/java/com/example/tenthread/controller/CommentController.java
+++ src/main/java/com/example/tenthread/controller/CommentController.java
@@ -18,18 +18,22 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
 
     private final CommentService commentService;
+
+    //댓글 작성
     @PostMapping("/comment/{postId}")
     public ApiResponseDto createComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentRequestDto requestDto, @PathVariable Long postId){
       log.info("createComment : "+ requestDto.getBody());
       return commentService.createComment(postId,requestDto,userDetails.getUser());
     }
 
+    //댓글 수정
     @PutMapping("/comment/{commentId}")
     public ApiResponseDto updateComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentRequestDto requestDto, @PathVariable Long commentId){
         log.info("updateComment : "+ requestDto.getBody());
         return commentService.updateComment(commentId,requestDto,userDetails.getUser());
     }
 
+    //댓글 삭제
     @DeleteMapping("/comment/{commentId}")
     public ApiResponseDto deleteComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long commentId){
         log.info("deleteComment : ");
@@ -37,6 +41,7 @@ public class CommentController {
         return commentService.deleteComment(commentId,userDetails.getUser());
     }
 
+    //예외 처리
     @ExceptionHandler({NullPointerException.class})
     public ResponseEntity<ApiResponseDto> handleException(NullPointerException ex) {
         ApiResponseDto restApiException = new ApiResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
