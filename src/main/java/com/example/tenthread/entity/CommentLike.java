3277f0cc06cfed074c5450cfc7c7531632baa16b
SonMyungJi <linolousky@gmail.com>
2023-07-17 21:02:05 +0900

    postLike 기능 추가

---
+++ src/main/java/com/example/tenthread/entity/CommentLike.java
@@ -0,0 +1,30 @@
+package com.example.tenthread.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "comment_like")
+public class CommentLike extends TimeStamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    public CommentLike(User user, Comment comment) {
+        this.user = user;
+        this.comment = comment;
+    }
+}
