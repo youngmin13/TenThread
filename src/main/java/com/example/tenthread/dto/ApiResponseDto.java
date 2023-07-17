9e1972da224f0b1c7238cb05d1ecba0773ae06d4
SonMyungJi <linolousky@gmail.com>
2023-07-17 16:48:13 +0900

    User 관련 API

--- src/main/java/com/example/tenthread/dto/ApiResponseDto.java
+++ src/main/java/com/example/tenthread/dto/ApiResponseDto.java
@@ -1,14 +1,10 @@
 package com.example.tenthread.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponseDto {
     private String msg;
     private Integer statusCode;
