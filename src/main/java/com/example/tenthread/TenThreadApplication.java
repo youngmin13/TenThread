7e0cb84bafdbcbb071991bcbd9541c7ac9391971
youngmin13 <wjsdudals56@naver.com>
2023-07-17 12:28:20 +0900

    init commit

---
+++ src/main/java/com/example/tenthread/TenThreadApplication.java
@@ -0,0 +1,13 @@
+package com.example.tenthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenThreadApplication.class, args);
+    }
+
+}
