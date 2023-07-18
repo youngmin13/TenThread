c9d42e5a4cdbe6c27e1d5cc865422411f9647b88
youngmin13 <wjsdudals56@naver.com>
2023-07-18 10:44:38 +0900

    profile 기능 주석

--- src/main/java/com/example/tenthread/controller/ProfileController.java
+++ src/main/java/com/example/tenthread/controller/ProfileController.java
@@ -1,18 +1,14 @@
 package com.example.tenthread.controller;
 
 import com.example.tenthread.dto.ApiResponseDto;
-import com.example.tenthread.dto.ProfilePasswordCheckDto;
 import com.example.tenthread.dto.ProfileRequestDto;
 import com.example.tenthread.dto.UserResponseDto;
 import com.example.tenthread.security.UserDetailsImpl;
 import com.example.tenthread.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 @RestController
 public class ProfileController {
@@ -23,11 +19,22 @@ public class ProfileController {
         this.userService = userService;
     }
 
+    /**
+     * 나의 프로필 정보
+     * @param userDetails : 현재 로그인한 유저
+     * @return : username (email)이랑 nickname
+     */
     @GetMapping("/profile")
     public UserResponseDto getMyProfile (@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return userService.getMyProfile(userDetails.getUser());
     }
 
+    /**
+     * 프로필 업데이트
+     * @param userDetails : 현재 로그인한 유저
+     * @param profileRequestDto : 프로필 변경시 필요한 정보 (닉네임, 예전 비번, 바꿀 비번)
+     * @return 프로필 변경 성공 or exception
+     */
     @PutMapping("/profile")
     public ResponseEntity<ApiResponseDto> updateProfile (@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                          @RequestBody ProfileRequestDto profileRequestDto) {
--- src/main/java/com/example/tenthread/dto/ProfileRequestDto.java
+++ src/main/java/com/example/tenthread/dto/ProfileRequestDto.java
@@ -6,6 +6,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProfileRequestDto {
+    /**
+     * 프로필 변경이 이루어질 때 필요한 정보
+     */
     private String nickname;
     private String oldPassword;
     private String newPassword;
