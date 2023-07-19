95a574ef97b7a336f1dcf389bf6aefb79b519443
SeungBum Baek <wer06099@naver.com>
2023-07-19 20:24:03 +0900

    WebController 변경으로 인한 로직 수정

--- src/main/java/com/example/tenthread/controller/WebController.java
+++ src/main/java/com/example/tenthread/controller/WebController.java
@@ -6,8 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/main")
 public class WebController {
 
     @GetMapping("/")
