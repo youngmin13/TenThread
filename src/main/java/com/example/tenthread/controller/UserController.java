9c095d90e9127137b2df2cfad436b9fa8bd78e2a
SonMyungJi <linolousky@gmail.com>
2023-07-19 09:12:40 +0900

    controller - 로그아웃 삭제

--- src/main/java/com/example/tenthread/controller/UserController.java
+++ src/main/java/com/example/tenthread/controller/UserController.java
@@ -49,17 +49,6 @@ public class UserController {
         return ResponseEntity.ok().body(new ApiResponseDto("로그인 성공", HttpStatus.CREATED.value()));
     }
 
-    @PostMapping("/logout")
-    public void logout(HttpServletRequest request,HttpServletResponse response) {
-        String token = jwtUtil.resolveToken(request);
-
-        if (token != null) {
-            jwtUtil.addToBlacklist(token);
-        }
-
-        response.setHeader(JwtUtil.AUTHORIZATION_HEADER, "");
-    }
-
     @GetMapping("/kakao/callback")
     public ResponseEntity<ApiResponseDto> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         String token = kakaoService.kakaoLogin(code);
