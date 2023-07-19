3218699dca3cf191b08eb72175f3ed739bd4601a
SonMyungJi <linolousky@gmail.com>
2023-07-19 18:51:19 +0900

    로그인 오류메시지 출력

--- src/main/java/com/example/tenthread/controller/UserController.java
+++ src/main/java/com/example/tenthread/controller/UserController.java
@@ -40,12 +40,7 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponseDto> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
-        try {
-            userService.login(requestDto, response);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(new ApiResponseDto("회원을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()));
-        }
-
+        userService.login(requestDto, response);
         return ResponseEntity.ok().body(new ApiResponseDto("로그인 성공", HttpStatus.CREATED.value()));
     }
 
--- src/main/java/com/example/tenthread/dto/LoginRequestDto.java
+++ src/main/java/com/example/tenthread/dto/LoginRequestDto.java
@@ -1,19 +1,11 @@
 package com.example.tenthread.dto;
 
-import com.example.tenthread.entity.UserRoleEnum;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginRequestDto {
-    @Pattern(regexp = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
     private String username;
-
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[_!#$%&'*+/=?`{|}~^.-])[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]{8,15}$",
-            message = "최소 8자 이상, 15자 이하로 이루어진 알파벳, 숫자, 특수문자로 구성되어야 합니다.")
     private String password;
-
-    private UserRoleEnum role;
 }
