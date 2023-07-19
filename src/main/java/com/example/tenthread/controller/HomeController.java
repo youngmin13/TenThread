09fed03bbd4fbbde8b3226e93959c3b8c540cfe5
SeungBum Baek <wer06099@naver.com>
2023-07-19 18:49:50 +0900

    게시글 생성 html 작업 및 post 로직 변경

---
+++ src/main/java/com/example/tenthread/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.tenthread.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/createPost")
+    public String createPost() {
+        return "createPost";
+    }
+}
