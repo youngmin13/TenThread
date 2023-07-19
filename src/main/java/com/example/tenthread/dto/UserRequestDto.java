78e9efd196d1dafdf865e26093f07859255284f1
SonMyungJi <linolousky@gmail.com>
2023-07-19 18:01:43 +0900

    회원가입성공 문구 출력 + 정규표현식 좀 더 자세하게 수정

--- src/main/java/com/example/tenthread/dto/UserRequestDto.java
+++ src/main/java/com/example/tenthread/dto/UserRequestDto.java
@@ -10,15 +10,15 @@ import lombok.Setter;
 public class UserRequestDto {
 
     @Pattern(regexp = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$",
-            message = "이메일 형식이어야 합니다. 예) tester123@gmail.com")
+            message = "username은 이메일 형식이어야 합니다. 예) tester123@gmail.com")
     private String username;
 
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[_!#$%&'*+/=?`{|}~^.-])[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]{8,15}$",
-            message = "최소 8자 이상, 15자 이하로 이루어진 알파벳, 숫자, 특수문자로 구성되어야 합니다.")
+            message = "password는 최소 8자 이상, 15자 이하로 이루어진 알파벳, 숫자, 특수문자로 구성되어야 합니다.")
     private String password;
 
     @Pattern(regexp = "^[a-z0-9]{4,10}$",
-            message = "최소 4자 이상, 10자 이하로 이루어진 알파벳 소문자와 숫자로 구성되어야 합니다.")
+            message = "nickname은 최소 4자 이상, 10자 이하로 이루어진 알파벳 소문자와 숫자로 구성되어야 합니다.")
     private String nickname;
 
     private UserRoleEnum role;
