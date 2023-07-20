cdeeebf461ffd793fdd2275c8079a27e16174645
youngmin13 <wjsdudals56@naver.com>
2023-07-20 20:32:30 +0900

    마이페이지 시도중

--- src/main/java/com/example/tenthread/controller/WebController.java
+++ src/main/java/com/example/tenthread/controller/WebController.java
@@ -52,4 +52,9 @@ public class WebController {
     public String getProfile() {
         return "profile";
     }
+
+    @GetMapping("/myPage")
+    public String myPage() {
+        return "myPage";
+    }
 }
